4671b98cddf70f625523cef7e389854eb1baf4d9
Francisco <matteomejia@github.com>
2019-06-13 12:40:22 -0500

    v0.1.0 untested

--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -12,8 +12,20 @@ public class Translator {
         Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
 
         List<String> texts = new LinkedList<>();
+        List<Language> languages = translate.listSupportedLanguages();
+
+        System.out.println("Lenguajes de salida:\n");
+        for (Language language : languages) {
+            System.out.printf("Nombre: %s, Codigo: %s\n", language.getName(), language.getCode());
+        }
+        System.out.println("Seleccione el lenguaje de salida:\n");
 
         Scanner scanner = new Scanner(System.in);
+        String endLanguage = scanner.nextLine();
+
+        System.out.println("\nIngrese el texto a traducir:\n");
+
+        scanner = new Scanner(System.in);
         texts.add(scanner.nextLine());
 
         List<Detection> detections = translate.detect(texts);
@@ -21,7 +33,7 @@ public class Translator {
 
         List<Translation> translations = translate.translate(texts,
                 Translate.TranslateOption.sourceLanguage(detection.getLanguage()),
-                Translate.TranslateOption.targetLanguage("de"));
+                Translate.TranslateOption.targetLanguage(endLanguage));
 
         Translation translation = translations.get(0);
         System.out.println(translation.getTranslatedText());
---
+++ src/test/java/TranslatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TranslatorTest {
+
+    @Test
+    public void main() {
+    }
+}
